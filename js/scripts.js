29c57de4110e866d6aa03913727cd4a174b85b69
Joshua Fairchild <Joshua.fairchild429@gmail.com>
2017-04-17 09:21:15 -0700

    add a ticker that displays the current score

--- js/scripts.js
+++ js/scripts.js
@@ -33,6 +33,7 @@ let head;
 function Snake() {
   this.direction = 'right';
   this.body = [];
+  this.score;
 }
 
 
@@ -83,7 +84,7 @@ Snake.prototype.move = function(sq, food) {
   let nextSpot;
   dir = this.direction;
 
-  //based off the direction, determine the coords of what the next spot is to be
+  //based on the direction, determine the coords of what the next spot is to be
   if (dir === 'right') {
     nextSpot = [(this.body[0][0])+10,this.body[0][1]];
   } else if (dir === 'left') {
@@ -95,29 +96,28 @@ Snake.prototype.move = function(sq, food) {
   }
 
   //kill the snake if necessary
-  //check for contact wall
+  //check for contact with wall
   if (nextSpot[0] <= -10 || nextSpot[0] >= 500 || nextSpot[1] <= -10 || nextSpot[1] >= 500) {
     // debugger;
-    die();
+    gameOver();
   }
-  //check for contact self
+  //check for contact with self
   for (let k = 0; k < this.body.length; k++) {
     if (nextSpot[0] === this.body[k][0] && nextSpot[1] === this.body[k][1]) {
       // debugger;
-      die();
+      gameOver();
     }
   }
 
-  //move all of the squares forward by one space
+  //move all of the Snake.body arrays (squares) forward by one space
   for (let i = this.body.length - 1; i > 0; i--) {
     this.body[i] = this.body[(i-1)];
   }
   this.body[0] = nextSpot;
-  console.log(dir);
 
   //draw the new snake and the current food
   c.clearRect(0,0,h,w);
-  // c.fillStyle='black';
+  // c.fillStyle='black';    /* uncommenting these two lines looks pretty cool */
   // c.fillRect(0,0,h,w);
   for (let j = 0; j < this.body.length; j++) {
     sq.draw(this.body[j][0],this.body[j][1]);
@@ -127,7 +127,7 @@ Snake.prototype.move = function(sq, food) {
 
 
 //----------recognize & handle the snake eating food----------------------------
-Snake.prototype.consumeFood = function(sq, food) {
+Snake.prototype.lookForFood = function(sq, food) {
   head = [this.body[0][0],this.body[0][1]];
   if (head[0] === food[0] && head[1] === food[1]) {
     this.grow(sq, food);
@@ -135,11 +135,19 @@ Snake.prototype.consumeFood = function(sq, food) {
 
     for (let i = 0; i < this.body.length; i++) {
       if (newFood[0] === this.body[i][0] && newFood[1] === this.body[i][1]) {
-        debugger;
+        // debugger;
         newFood = spawnFood(sq);
       }
     }
   }
+}
+
+
+Snake.prototype.getScore = function() {
+  let unformattedScore = this.body.length - 8;
+  let str = "" + unformattedScore;
+  let pad = "000";
+  this.score = pad.substring(0, pad.length - str.length) + str;
 }
 
 
@@ -201,30 +209,28 @@ function spawnFood(sq) {
 
 
 //----------fn to call when the snake hits an invalid spot----------------------
-function die() {
+function gameOver() {
   location.reload();
 }
 
 
-//----------fn to init and begin running the game-------------------------------
+//----------fn to init and and begin looping the game---------------------------
 function game() {
   getDirection(snake);
   snake.init(square);
 
   setInterval(function() {
     snake.move(square, newFood);
-    snake.consumeFood(square, newFood);
+    snake.lookForFood(square, newFood);
+    snake.getScore();
+    $('#score-span').text(snake.score);
     // debugger;
   }, frameRate)
 }
 
 
 
-                      /*-----------------
-                   --------UI maybe?--------
-                      -----------------*/
 
-//----------start a new game----------------------------------------------------
 game();
 
 
@@ -239,3 +245,5 @@ game();
 // 2. option to swap black and white (looks nice both ways)
 // 3. make it so you can't suicide the snake by arrowing too quick
 // 4. make it pretty
+// 5. make starting direction random
+// 6. make starting food random
